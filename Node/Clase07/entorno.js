a00378192f1b659359a7c7853018fff39fa219eb
Gomilarocio99 <gomilarocio99@gmail.com>
2025-11-11 00:15:46 -0300

    Adjunto clase 07 node

---
+++ Node/Clase07/entorno.js
@@ -0,0 +1,6 @@
+let nombre = Process.env.NOMBRE || "Sin Nombre";
+let web = process.env.MI_WEB || "No tengo web"
+console.log("¡Hola " + nombre + "!");
+console.log("Mi web es: "+ web);
+
+console.log("Ultima ejecucion");
